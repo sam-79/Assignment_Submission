65ab9f03668db7856e766241bac5ce517b6c893c
Sameer Borkar <63750493+sam-79@users.noreply.github.com>
2021-07-27 23:45:06 +0530

    Create Day1_Assignment.py

---
+++ Day1_Assignment.py
@@ -0,0 +1,11 @@
+
+#List sorting in descending order
+
+int_list = [74,4,2,7,55,76,47478,4,21,124,42,4,4,36,6,8,0,95,6]
+int_list.sort(reverse=True)
+print(int_list)
+
+
+alpha_list=['t','o','i','q','z','m','v','p','r']
+alpha_list.sort(reverse=True)
+print(alpha_list)
